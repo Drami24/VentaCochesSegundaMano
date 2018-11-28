283a5551831f8560d8403b1680e46a15130ae283
a18franciscorm <chiscazo.paco@gmail.com>
2018-11-28 13:43:28 +0100

    Adicion a o TableView Funcional.

---
+++ EmpresaSegundaMano/src/vista/ClienteP.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+/**
+ *
+ * @author a18franciscorm
+ */
+public class ClienteP {
+    String id;
+    String nombre;
+    String fecha;
+
+    public ClienteP() {
+    }
+
+    public ClienteP(String id, String nombre, String fecha) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fecha = fecha;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
+}
--- EmpresaSegundaMano/src/vista/ControladorVentana.java
+++ EmpresaSegundaMano/src/vista/ControladorVentana.java
@@ -6,23 +6,28 @@
 package vista;
 
 import java.net.URL;
+import pojos.Cliente;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  *
  * @author a18franciscorm
  */
 public class ControladorVentana implements Initializable{
-    String vacio="";
+    String vaciadorString="";
+    LocalDate vaciadorAlta=null;
     
     @FXML
     private TextField texId;
@@ -44,34 +49,67 @@ public class ControladorVentana implements Initializable{
     
     @FXML
     private TableColumn fecha;
+    private ObservableList <ClienteP>listaproveedores = FXCollections.observableArrayList();
     
     @FXML
     private void darAlta(ActionEvent event ){
-        
+        String auxid=null,auxname=null,auxdata=null;
         System.out.println("Click en alta, datos: ");
         if(!texId.getText().isEmpty()){
             System.out.println(texId.getText());
-            texId.setText(vacio);}
-        else
+            auxid=texId.getText();
+            texId.setText(vaciadorString);}
+        else{
             System.out.println("No tiene id");
+            auxid="nada";
+        }
         if(!texName.getText().isEmpty()){
             System.out.println(texName.getText());
-            texName.setText(vacio);
+            auxname=texName.getText();
+            texName.setText(vaciadorString);
         }
-        else
+        else{
             System.out.println("No tiene nombre");
+            auxname="nada";
+        }
         try{
             System.out.println(fechaAlta.getValue().toString());
+            auxdata=fechaAlta.getValue().toString();
+            fechaAlta.setValue(vaciadorAlta);
         }
         catch(RuntimeException rte1){
             System.out.println("Non se introduxo Data");
+            auxdata="nada";
         }
+        
+        ClienteP e=new ClienteP(auxid,auxname,auxdata);
+        listaproveedores.add(e);
+        refrescar(listaproveedores);
     }
     
+    @FXML
+    private void darBaja(ActionEvent event ){
+        
+        System.out.println("Click en Baja");
+        
+    }
+    
+    @FXML
+    private void Modificar (ActionEvent event ){
+        
+        System.out.println("Click en modificar");
+        
+    }
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Buenas, me he iniciado.");
+        id.setCellValueFactory(new PropertyValueFactory("id"));
+        nombre.setCellValueFactory(new PropertyValueFactory("nombre"));
+        fecha.setCellValueFactory(new PropertyValueFactory("fecha"));
+    }
+    public void refrescar(ObservableList ol){
+        tablaProveedores.setItems(ol);
     }
     
 }
